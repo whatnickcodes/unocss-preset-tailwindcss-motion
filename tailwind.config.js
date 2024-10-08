f7885ce72984c509272888a09422a0558268c917
Nicholas V. Cerminara <nick@landslide.digital>
2024-10-08 18:08:34 -0400

    first

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    mode: 'jit',
+    darkMode: 'class',
+    content: ['./compare-test/index.html'],
+    theme: {
+        extend: {},
+    },
+    plugins: [require('tailwindcss-motion')],
+}
