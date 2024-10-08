ed5f307073e9832c2ecd0f4e6448df2798915d46
Nicholas V. Cerminara <nick@landslide.digital>
2024-10-08 19:27:15 -0400

    quick

--- uno.config.js
+++ uno.config.js
@@ -1,8 +1,31 @@
-import presetUno from '@unocss/preset-uno'
 import { defineConfig } from 'unocss'
+import { presetUno } from 'unocss'
+
+
+// Not ideal but it works...
+import fs from 'fs'
+import path from 'path'
+const resetCSS = fs.readFileSync(
+  path.resolve(process.cwd(), 'node_modules/@unocss/reset/tailwind.css'),
+  'utf-8'
+)
+
 
 export default defineConfig({
     presets: [
         presetUno(),
     ],
+    content: {
+        filesystem: ['./compare-test/index.html']
+    },
+    preflights: [
+        {
+            getCSS: () => `
+                ${resetCSS}
+                nav {
+                    border-top: 8px solid orange;
+                }
+            `
+        }
+    ]
 })
